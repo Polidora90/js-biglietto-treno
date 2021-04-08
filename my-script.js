59645468bc1125c96fd24c5da980dbed20323a47
Polidora90 <alessandra.torry@gmail.com>
2021-04-08 19:35:11 +0200

    aggiunge un controllo sui dati

--- my-script.js
+++ my-script.js
@@ -1,17 +1,28 @@
-var lunghezzaPercorso = prompt ("Inserisci il numero di chilometri che desideri percorrere");
+var lunghezzaPercorso = parseInt (prompt ("Inserisci il numero di chilometri che desideri percorrere"));
 console.log (lunghezzaPercorso + "Km");
 
-var anniUtente = prompt ("Quanti anni hai?");
+if (Number.isNaN(lunghezzaPercorso)) {
+    alert ("Carattere non valido - Inserire caratteri validi")
+}
+
+var anniUtente = parseInt (prompt ("Quanti anni hai?"));
 console.log (anniUtente + " anni");
 
+if (Number.isNaN(anniUtente)) {
+    alert ("Carattere non valido - Inserire caratteri")
+}
+
 var prezzoBiglietto = lunghezzaPercorso * 0.21;
+console.log (prezzoBiglietto + "€")
 
 if (anniUtente < 18) {
     prezzoBiglietto = prezzoBiglietto * 0.8;
+    console.log ("Sconto 20%")
 }
 
 if (anniUtente >= 65) {
     prezzoBiglietto = prezzoBiglietto * 0.6;
+    console.log ("Sconto 40%")
 }
 
 var prezzoFinaleBiglietto = prezzoBiglietto.toFixed(2);
